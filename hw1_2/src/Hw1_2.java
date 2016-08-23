ee99d9cbc9a9cfd775ed8d51047d632c83a801aa
Panpech Pothong <PanpechMac@Panpechs-MacBook-Pro.local>
2016-08-23 15:15:17 +0700

    add some code

--- hw1_2/src/Hw1_2.java
+++ hw1_2/src/Hw1_2.java
@@ -4,16 +4,20 @@
 public class Hw1_2 {
     float gpaCalculation(int[] credit, int[] grade)
     {
-        /**
-         * your code here
-         */
-        return 0.0f;
+        float sum=0;
+        float sumcre=0;
+        for(int i=0;i<grade.length;i++)
+        {
+            sum = sum+ (grade[i]*credit[i]);
+            sumcre = credit[i]+sumcre;
+        }
+        return sum/sumcre;
     }
 
     public static void main(String[] args) {
         int[] credit = {3, 3, 3, 3, 2};
         int[] grade  = {2, 4, 4, 3, 2};
         Hw1_2 h = new Hw1_2();
-        System.out.printf("Your GPA is %f\n", h.gpaCalculation(credit, grade));
+        System.out.printf("Your GPA is %.2f\n", h.gpaCalculation(credit, grade));
     }
 }
